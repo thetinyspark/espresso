cf23ff414b0c69dbf315618092f9f7ce847b86c3
thetinyspark <contact@the-tiny-spark.com>
2021-10-12 22:07:34 +0200

    add inventory class and IStorable interface

--- lib/index.ts
+++ lib/index.ts
@@ -5,10 +5,14 @@ import Grid2D from "./model/space/partitioning/grid/Grid2D";
 import Grid3D from "./model/space/partitioning/grid/Grid3D";
 import {screenToIso, isoToScreen, isoSort} from "./utils/iso.utils";
 import IsoMap from "./view/map/IsoMap";
+import Inventory from "./model/space/storage/Inventory";
+import IStorable from "./model/space/storage/IStorable";
 
 
 
 export {
+    Inventory, 
+    IStorable,
     Isometric,
     makeIsometric,
     GameNode, 
